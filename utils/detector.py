a281a6614fda6a4b63d087563c8ff90d394900a5
abhinav-singh0 <ee25mt010@iitdh.ac.in>
2026-05-11 10:11:55 +0530

    Update detector.py

--- utils/detector.py
+++ utils/detector.py
@@ -9,7 +9,7 @@ model = YOLO("best.pt")
 def detect_vehicles(image_path):
 
     # Run prediction
-    results = model.predict(source=image_path, conf=0.5)
+    results = model.predict(source=image_path, conf=0.5,imgsz=320)
 
     result = results[0]
 
